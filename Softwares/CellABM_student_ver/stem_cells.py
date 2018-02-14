0d51474c7b42dd9fbdff5ebccf88ca48a5627e62
HarryCooper <hpcooper1@sheffield.ac.uk>
2018-02-14 10:34:33 +0000

    Got growth and mitosis to work correctly for EC's (I believe)

--- Softwares/CellABM_student_ver/stem_cells.py
+++ Softwares/CellABM_student_ver/stem_cells.py
@@ -35,11 +35,13 @@ class sc(general_cell):
     #uses general_cell apoptosis and kill_cell methods
 
     def split_cell(self,env):
-        newcellpos=[self.pos[0]+random.uniform(-1,1)*self.radius, self.pos[1]+random.uniform(-1,1)* self.radius]
-        newcell= sc(ID=self.num_sc, stage=1, pos=newcellpos, direc=random.random()*2*math.pi, turnover=1, radius=math.sqrt(self.area/math.pi), area=self.area/2)
-        #self.radius = self.radius/2 #Daughter cell is half area of parent
         self.area = self.area/2 #Daughter cell is halve the size of the parent
         self.radius = math.sqrt(self.area/math.pi)#Daughter cell is halve the size of the parent
+        newcellpos=[self.pos[0]+random.uniform(-1,1)*self.radius, self.pos[1]+random.uniform(-1,1)* self.radius]
+        newcell= sc(ID=self.num_sc, stage=1, pos=newcellpos, direc=random.random()*2*math.pi, turnover=1, radius=self.radius, area=self.area)
+        #self.radius = self.radius/2 #Daughter cell is half area of parent
+#        self.area = self.area/2 #Daughter cell is halve the size of the parent
+#        self.radius = math.sqrt(self.area/math.pi)#Daughter cell is halve the size of the parent
         self.stage = 1
         self.turnover = self.turnover +1
         self.pos=[self.pos[0]+random.uniform(-1,1)*self.radius, self.pos[1]+random.uniform(-1,1)* self.radius]
@@ -71,17 +73,19 @@ class sc(general_cell):
         print("*1*", "CellID:", self.ID, "Stage:", self.stage, "Area:", self.area, "Radius:", self.radius)
         
         if self.stage == 1: #Increase orignial size by 1/4
-            self.area = self.area + (self.area)/4 
-            print(self.area)
+            self.area = self.area * 1.25
+#            print(self.area)
         elif self.stage == 2: #Decrease by 1/4 to achieve orignial, then increase by 2/4
-            self.area = (self.area-(self.area)/4) + (self.area+(self.area)/2)
-            print(self.area)
+            sa = self.area / 1.25
+            self.area = sa * 1.5
+#            print(self.area)
         elif self.stage == 3: #Decrease by 2/4 to achieve orignial, the increase by 3/4
-            self.area = (self.area-(self.area)/2) + (self.area+(3*(self.area))/4)
-            print(self.area)
+            sa = self.area / 1.5
+            self.area = sa * 1.75
+#            print(self.area)
         else: #Decrease by 3/4 to achieve orignial, then double
-            self.area = (self.area-(3*(self.area))/4) + (self.area+2*(self.area))
-            print(self.area)
+            sa = self.area / 1.75
+            self.area = sa * 2
         #Send this result onto mitosis.
         self.radius = math.sqrt(self.area/math.pi)
         
