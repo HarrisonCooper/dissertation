c9f16f467c48d4d2207e168a2c422e161ea9f8e4
HarryCooper <hpcooper1@sheffield.ac.uk>
2018-02-26 19:04:01 +0000

    Got coloours working correctly on growth_curve

--- Softwares/CellABM_student_ver/results.py
+++ Softwares/CellABM_student_ver/results.py
@@ -11,6 +11,7 @@ import numpy as np
 import pylab
 from cancer_cells import cc
 from stem_cells import sc
+from quiescent_cells import qc
 import os
 import operator
 #import matplotlib.animation as animation
@@ -75,19 +76,24 @@ def plot_2d(env, directory, labels, n_it):
     
     for n in range (len(env.stemcells)):
         
-        print(n, env.stemcells[n].radius)
+#        print(n, env.stemcells[n].radius)
         
         ax.add_artist(Circle((env.stemcells[n].pos[0], env.stemcells[n].pos[1]), env.stemcells[n].radius, fc='r', alpha = 0.5))
         if labels == True:            
             ax.text(env.stemcells[n].pos[0]-0.7, env.stemcells[n].pos[1]-0.5, str(env.stemcells[n].ID), fontsize=7)     #IDs    
+            
+    for n in range (len(env.quiescentcells)):
+        ax.add_artist(Circle((env.quiescentcells[n].pos[0], env.quiescentcells[n].pos[1]), env.quiescentcells[n].radius, fc='b', alpha = 0.5))
+        if labels == True:            
+            ax.text(env.quiescentcells[n].pos[0]-0.7, env.quiescentcells[n].pos[1]-0.5, str(env.quiescentcells[n].ID), fontsize = 7)     #IDs   
     
     plt.axis([0, env.size, 0, env.size])
     
     if n_it == 0:
-        figname = 'Initial Setup \n No of Cancer cells = %s \n No of Stem cells = %s' %(str(cc.num_cc), str(sc.num_sc))
+        figname = 'Initial Setup \n No of Cancer cells = %s \n No of Stem cells = %s \n No of Quiescent cells = %s' %(str(cc.num_cc), str(sc.num_sc), str(qc.num_qc))
         filename = 'Initial_Setup' 
     else:
-        figname = 'Iteration %s \n No of Cancer cells = %s \n No of Stem cells = %s' %(str(n_it), str(cc.num_cc), str(sc.num_sc))
+        figname = 'Iteration %s \n No of Cancer cells = %s \n No of Stem cells = %s \n No of Quiescent cells = %s' %(str(n_it), str(cc.num_cc), str(sc.num_sc), str(qc.num_qc))
         filename = "Iteration_" + str(n_it)
     
     ax.set_title(figname)
@@ -97,9 +103,10 @@ def plot_2d(env, directory, labels, n_it):
     #Can adapt to have time to heal over number of senescent cells / age.
 def growth_curve(num_cells,directory):
     fig=plt.figure()
-    plt.plot(num_cells[0,:],'g',label='cancer cells')
+    plt.plot(num_cells[0,:],'g',label='Senescent cells')
     plt.hold(True)
-    plt.plot(num_cells[1,:],'b',label='stem cells')
+    plt.plot(num_cells[1,:],'r',label='Stem cells')
+    plt.plot(num_cells[2,:],'b',label='Quiescent cells')
     fig.suptitle('growth_curve')
     plt.xlabel('interation')
     plt.ylabel('number of cells')
